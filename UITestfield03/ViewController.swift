027404b54f758915ca6fa941171f77196e0cdcbb
D7703_23 <d7703_23@d7703-23ui-imac.local>
2019-04-04 12:48:08 +0900

    UITextField

--- UITestfield03/ViewController.swift
+++ UITestfield03/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+//1번 delegate 상속받기
+class ViewController: UIViewController, UITextFieldDelegate {
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //2번 커넥션
+        myTextField.delegate = self
+        myTextField.clearButtonMode = UITextField.ViewMode.always
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        let inString = myTextField.text
+        
+        print(inString!)
+        myLabel.text = inString
+        myTextField.text = ""
+        myTextField.resignFirstResponder()
+    }
+    
+    //delegate method
+    // called when 'return' key pressed. return NO to ignore.
+    //3번 가져오기
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        myTextField.resignFirstResponder()
+        myTextField.backgroundColor = UIColor.yellow
+        return true
+    }
+    
+    public func textFieldShouldClear(_ textField: UITextField) -> Bool{
+        view.backgroundColor = UIColor.green
+        return true
+    }
+    
 }
 
